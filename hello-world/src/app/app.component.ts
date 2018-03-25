452aa45c601655c713115a6c288e6ae52f023aba
Eric Smith <easmith03@comcast.net>
2018-03-25 16:58:23 -0400

    More Files

---
+++ hello-world/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { IButton } from 'selenium-webdriver';
+import {HttpClient, HttpParams, HttpRequest, HttpEvent, HttpHeaders} from '@angular/common/http';
+import { Input } from '@angular/compiler/src/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+
+})
+export class AppComponent {
+  title = 'Hello World App';
+  uploadFile: File = null;
+  firstName: string = 'Dudley';
+  lastName: string = 'Nowrite';
+
+  constructor(private http: HttpClient) { }
+
+  handleFileInput(files: FileList) {
+    //this.fileToUpload = files.item(0);
+    this.uploadFile = files[0]
+    console.log('handleFileInput', this.uploadFile);
+  }  
+
+  handleFirstNameInput(formChange: any) {
+    //this.fileToUpload = files.item(0);
+    //console.log('handleFirstNameInput', formChange);
+    console.log('handleFirstNameInput', formChange.value);
+    //console.log('handleFirstNameInput', formChange.firstName);
+    //console.log('handleFirstNameInput', formChange.lastName);
+    //console.log('handleFirstNameInput', firstName);
+    this.firstName = formChange.value;
+  }  
+  handleLastNameInput(formChange: any) {
+    this.lastName = formChange.value;
+  } 
+
+//  handleFormSubmit(form: FormData) {
+//    //this.fileToUpload = files.item(0);
+//    console.log('form', FormData);
+//  }  
+
+  submitButton(button: IButton) {
+    //this.fileToUpload = files.item(0);
+    console.log('button', button);
+
+    let formData = new FormData();
+    formData.append('image', this.uploadFile);
+    formData.append('lname', this.lastName);
+    formData.append('fname', this.firstName);
+
+    let params = new HttpParams();
+    let headers = new HttpHeaders(); //{'Content-Type': 'application/x-www-form-urlencoded'}
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    //let headers = new Headers({ 'Content-Type': 'application/json' });
+   // let headers: {'Content-Type': 'application/x-www-form-urlencoded'};
+    const options = {
+      params: params,
+      headers: headers
+    };
+
+    console.log('submit');
+    const req = new HttpRequest('POST', "http://localhost:8081/imageUpload", formData, options);
+    this.http.request(req)
+    .subscribe(function (event) {
+        console.log('debug2', event);
+    });
+
+  }  
+
+}
